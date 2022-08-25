ba77cc3051e5849b25ef1ab67cd52c153bb84404
Arpan <arpankarjee2021@gmail.com>
2022-08-25 22:13:05 +0530

    first commit

---
+++ js/script.js
@@ -0,0 +1,29 @@
+navbar = document.querySelector('.header .flex .navbar');
+profile = document.querySelector('.header .flex .profile');
+
+
+document.querySelector('#menu-btn').onclick = () =>{
+    navbar.classList.toggle('active');
+    profile.classList.remove('active');
+}
+
+
+document.querySelector('#user-btn').onclick = () =>{
+    profile.classList.toggle('active');
+    navbar.classList.remove('active');
+}
+
+window.onscroll = () =>{
+    navbar.classList.remove('active');
+    profile.classList.remove('active');
+}
+
+function loader(){
+    document.querySelector('.loader').style.display = 'none';
+}
+
+function fadeOut(){
+    setInterval(loader,500);
+}
+
+window.onload = fadeOut;
